ce99d65f6b89265e06bd2ab2bbe7865669bd9571
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-05-13 16:10:31 -0300

    várias atualizações

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,25 +1,37 @@
 // import { connectToDatabase } from "../../lib/connectToDatabase";
 import clientPromise from "../../lib/mongodb"
 
-export default async function handler(request, response){
+export default async function handler(request, response) {
+  if (request.method === "GET") {
     try {
-        // const { mongoClient } = await connectToDatabase();
-        const mongoClient = await clientPromise;
-        const db = mongoClient.db("loja_carros");
-        const collection = db.collection("carros");
-        const results = await collection
-          .find({})
-          .project({
-            grades: 0,
-            borough: 0,
-            carro_id: 0,
-          })
-          .limit(10)
-          .toArray();
+      // const { mongoClient } = await connectToDatabase();
+      const mongoClient = await clientPromise;
+      const db = mongoClient.db("loja_carros");
+      const collection = db.collection("carros");
+      const results = await collection
+        .find({})
+        .project({
+          grades: 0,
+          borough: 0,
+          carro_id: 0,
+        })
+        .limit(10)
+        .toArray();
 
-        response.status(200).json(results);  
+      response.status(200).json(results);
     } catch (e) {
       console.error(e);
-      response.status(500).json(e);  
+      response.status(500).json(e);
     }
+  }
+  else if (request.method === "POST") {
+    const data = request.body;
+    const mongoClient = await clientPromise;
+    const db = mongoClient.db("loja_carros");
+    const yourCollection = db.collection("carros");
+    const results = await yourCollection.insertOne(data);
+    console.log(results);
+    //client.close();
+    response.status(201).json({ message: "Data inserted successfully!" });
+  }
 }
